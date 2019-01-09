9a846460c97e0a6ff8262aaa9201ff02681c3b05
Jeremy Soller <jeremy@system76.com>
2019-01-09 14:30:55 -0700

    Add sources

--- build.sh
+++ build.sh
@@ -15,6 +15,10 @@ do
     echo "deb http://archive.ubuntu.com/ubuntu $dist-security main" >> build/mirror.list
     echo "deb http://archive.ubuntu.com/ubuntu $dist-updates main" >> build/mirror.list
     echo "deb http://archive.ubuntu.com/ubuntu $dist-proposed main" >> build/mirror.list
+    echo "deb-src http://archive.ubuntu.com/ubuntu $dist main" >> build/mirror.list
+    echo "deb-src http://archive.ubuntu.com/ubuntu $dist-security main" >> build/mirror.list
+    echo "deb-src http://archive.ubuntu.com/ubuntu $dist-updates main" >> build/mirror.list
+    echo "deb-src http://archive.ubuntu.com/ubuntu $dist-proposed main" >> build/mirror.list
 done
 
 echo "clean http://archive.ubuntu.com/ubuntu" >> build/mirror.list
