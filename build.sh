d9f0c86324a2f3445dec1630c2393cf359b3b6ce
Jeremy Soller <jeremy@system76.com>
2019-01-09 15:47:02 -0700

    Add restricted and multiverse, since they are pretty small

--- build.sh
+++ build.sh
@@ -9,21 +9,25 @@ echo "set nthreads $(nproc)" >> build/mirror.list
 echo "set _autoclean 1" >> build/mirror.list
 echo "set run_postmirror 0" >> build/mirror.list
 
-for dist in bionic cosmic disco
+ARCHIVE=http://archive.ubuntu.com/ubuntu
+COMPONENTS=(main restricted multiverse)
+DISTS=(bionic cosmic disco)
+
+for dist in "${DISTS[@]}"
 do
-    echo "deb http://archive.ubuntu.com/ubuntu $dist main" >> build/mirror.list
-    echo "deb http://archive.ubuntu.com/ubuntu $dist-updates main" >> build/mirror.list
-    echo "deb http://archive.ubuntu.com/ubuntu $dist-security main" >> build/mirror.list
-    echo "deb http://archive.ubuntu.com/ubuntu $dist-backports main" >> build/mirror.list
-    echo "deb http://archive.ubuntu.com/ubuntu $dist-proposed main" >> build/mirror.list
-    echo "deb-src http://archive.ubuntu.com/ubuntu $dist main" >> build/mirror.list
-    echo "deb-src http://archive.ubuntu.com/ubuntu $dist-updates main" >> build/mirror.list
-    echo "deb-src http://archive.ubuntu.com/ubuntu $dist-security main" >> build/mirror.list
-    echo "deb-src http://archive.ubuntu.com/ubuntu $dist-backports main" >> build/mirror.list
-    echo "deb-src http://archive.ubuntu.com/ubuntu $dist-proposed main" >> build/mirror.list
+    echo "deb $ARCHIVE $dist ${COMPONENTS[@]}" >> build/mirror.list
+    echo "deb $ARCHIVE $dist-updates ${COMPONENTS[@]}" >> build/mirror.list
+    echo "deb $ARCHIVE $dist-security ${COMPONENTS[@]}" >> build/mirror.list
+    echo "deb $ARCHIVE $dist-backports ${COMPONENTS[@]}" >> build/mirror.list
+    echo "deb $ARCHIVE $dist-proposed ${COMPONENTS[@]}" >> build/mirror.list
+    echo "deb-src $ARCHIVE $dist ${COMPONENTS[@]}" >> build/mirror.list
+    echo "deb-src $ARCHIVE $dist-updates ${COMPONENTS[@]}" >> build/mirror.list
+    echo "deb-src $ARCHIVE $dist-security ${COMPONENTS[@]}" >> build/mirror.list
+    echo "deb-src $ARCHIVE $dist-backports ${COMPONENTS[@]}" >> build/mirror.list
+    echo "deb-src $ARCHIVE $dist-proposed ${COMPONENTS[@]}" >> build/mirror.list
 done
 
-echo "clean http://archive.ubuntu.com/ubuntu" >> build/mirror.list
+echo "clean $ARCHIVE" >> build/mirror.list
 
 mkdir -p build/var
 
