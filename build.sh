576b641300542e4147365395c72d3a6d452d722d
Jeremy Soller <jackpot51@gmail.com>
2022-09-22 11:41:32 -0600

    Use faster Ubuntu mirror as source

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Ubuntu default mirror
+#MIRROR=rsync://us.archive.ubuntu.com/ubuntu/
+
+# Fast mirror that moves load off of Ubuntu
+MIRROR="rsync://mirror.math.princeton.edu/pub/ubuntu/"
+
 set -ex
 
 mkdir -p build
@@ -12,7 +18,8 @@ RSYNC_ARGS=(
   --safe-links
   --hard-links
   --stats
-  rsync://archive.ubuntu.com/ubuntu/
+  --verbose
+  "${MIRROR}"
   build/
 )
 
