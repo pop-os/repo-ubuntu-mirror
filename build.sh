551b303df5a68947f6d52f743913b4880c32e58e
Jeremy Soller <jackpot51@gmail.com>
2022-09-22 11:48:38 -0600

    Use default US mirror

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-# Ubuntu default mirror
-#MIRROR=rsync://us.archive.ubuntu.com/ubuntu/
-
-# Fast mirror that moves load off of Ubuntu
-MIRROR="rsync://mirror.math.princeton.edu/pub/ubuntu/"
+# Ubuntu default US mirror
+MIRROR=rsync://us.archive.ubuntu.com/ubuntu/
 
 set -ex
 
