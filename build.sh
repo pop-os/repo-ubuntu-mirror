8c3a74c93b9ea7d1b8ad5b3a1aa84368c8ad31e7
Jeremy Soller <jackpot51@gmail.com>
2026-05-04 12:32:44 -0600

    Go back to princeton mirror, others are too slow

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@
 #MIRROR=rsync://us.archive.ubuntu.com/ubuntu/
 
 # Fast mirror that moves load off of Ubuntu
-MIRROR="rsync://mirrors.mit.edu/ubuntu/"
-#MIRROR="rsync://mirror.math.princeton.edu/pub/ubuntu/"
+MIRROR="rsync://mirror.math.princeton.edu/pub/ubuntu/"
 
 set -ex
 
